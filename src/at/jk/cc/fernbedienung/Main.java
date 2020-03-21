c8ecc123394f1878eb6781613e8100f21d08f3ca
jkueng83 <johannes.kueng@gmx.at>
2020-03-21 10:59:24 +0100

    Add "noch ein Text für GIT"

--- src/at/jk/cc/fernbedienung/Main.java
+++ src/at/jk/cc/fernbedienung/Main.java
@@ -3,5 +3,6 @@ package at.jk.cc.fernbedienung;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fernbedieung");
+        System.out.println("noch ein Text für GIT");
     }
 }
